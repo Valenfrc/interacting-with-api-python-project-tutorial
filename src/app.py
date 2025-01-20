47a70663cc1ed0222645ba0bc362d982de8ea399
Valenfrc <val21.frc@gmail.com>
2025-01-20 23:22:28 +0000

    api

--- src/app.py
+++ src/app.py
@@ -14,10 +14,24 @@ client_secret = os.environ.get("CLIENT_SECRET")
 
 
 birdy_uri = 'spotify:artist:63QpuNHIZ8APaK37LkV8Xd'
+
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 results = spotify.artist_albums(birdy_uri, album_type='album')
 albums = results['items']
 while results['next']:
     results = spotify.next(results)
+
+
     albums.extend(results['items'])
+    Mentor HQ
+17:14
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+con = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+artist_id = '09xj0S68Y1OU1vHMCZAIvz'
+
+response = con.artist_top_tracks(artist_id)
+
+print(response)
